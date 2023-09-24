0bbe22df438f2c79513a30b74754ac4838a7472c
Benton Li <cl2597@cornell.edu>
2023-09-24 19:48:34 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Pinecone - Vercel AI SDK Example",
-  description: "Pinecone - Vercel AI SDK Example",
+  title: "HypeR - Demo",
+  description: "HypeR - Demo",
 };
 
 import "../global.css";
