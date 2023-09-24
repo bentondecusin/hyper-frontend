6fc789a592702e9c7ffde4f5620f647e90cbf7a1
Benton Li <cl2597@cornell.edu>
2023-09-24 19:48:16 -0400

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,27 +1,40 @@
 import Image from "next/image";
-import PineconeLogo from "../../../public/pinecone.svg";
-import VercelLogo from "../../../public/vercel.svg";
+import Link from "next/link";
+import { AiFillGithub, AiOutlineInfoCircle } from "react-icons/ai";
 
 export default function Header({ className }: { className?: string }) {
   return (
     <header
       className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}
     >
-      <Image
-        src={PineconeLogo}
-        alt="pinecone-logo"
-        width="230"
-        height="50"
-        className="ml-3"
-      />{" "}
-      <div className="text-4xl ml-3 mr-3">+</div>
-      <Image
-        src={VercelLogo}
-        alt="vercel-logo"
-        width="160"
-        height="50"
-        className="mr-3 mt-3"
-      />
+      <Link
+        href="https://arxiv.org/pdf/2203.14692.pdf"
+        className="text-4xl ml-3 mr-3"
+      >
+        <p>HypeR Demo</p>
+      </Link>
+
+      <Link
+        href="https://github.com/sainyam/Hyper-Code"
+        aria-label={"Hyper-Code Repo"}
+        target="_blank"
+      >
+        <div className="flex items-center justify-between">
+          <div className="mr-3">
+            <AiFillGithub size={"50px"} />
+          </div>
+        </div>
+      </Link>
+      {/* <button
+        onClick={() => {
+          window.open(
+            ".com/pinecone-io/pinecone-vercel-starter",
+            "_blank"
+          );
+        }}
+        className="fixed right-20 top-4 md:right-20 md:top-6 text-xl text-white"
+      >
+      </button> */}
     </header>
   );
 }
