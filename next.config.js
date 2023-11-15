6e8ae264b7b2c51ff3b669c3294335b1d776cbd7
Benton Li <cl2597@cornell.edu>
2023-11-15 17:31:17 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:5000/api/:path*"
-            : "/api/",
+        // destination:
+        //   process.env.NODE_ENV === "development"
+        //     ? "http://127.0.0.1:5000/api/:path*"
+        //     : "/api/",
+        destination: "http://127.0.0.1:5000/api/:path*",
       },
     ];
   },
